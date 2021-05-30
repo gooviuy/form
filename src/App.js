400b757a2348baa149a7d02cedd48cecf4e0a2f5
gooviuy <gooviuy@outlook.com>
2021-05-29 22:38:36 -0300

    formulario

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,53 @@
+import React, { Fragment, useState } from "react";
+
+const Formulario = () => {
+  const [datos, setDatos] = useState({
+    nombre: "",
+    apellido: "",
+  });
+
+  const handleInputChange = (event) => {
+    setDatos({
+      ...datos,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const enviarDatos = (event) => {
+    event.preventDefault();
+    console.log(datos.nombre + " " + datos.apellido);
+  };
+
+  return (
+    <Fragment>
+      <h1>Formulario</h1>
+      <form className="row" onSubmit={enviarDatos}>
+        <div className="col-md-3">
+          <input
+            placeholder="Ingresar Nombre"
+            className="form-control"
+            type="text"
+            name="nombre"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="col-md-3">
+          <input
+            placeholder="Ingresar Apellido"
+            className="form-control"
+            type="text"
+            name="apellido"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="col-md-3">
+          <button className="btn btn-primary" type="submit">
+            Enviar
+          </button>
+        </div>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Formulario;
